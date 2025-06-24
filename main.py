b0011815ec0da10a3e1444d87fc92298909cc8ce
Tanushka Sharma <tanushkas723@gmail.com>
2025-06-24 16:53:08 +0530

    format

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ from services.summarizer import summarize_text
 from services.translator import translate_text
 from services.email_writer import write_email
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ChatbotRequest(BaseModel):
     prompt: str
--- services/chatbot.py
+++ services/chatbot.py
@@ -1,25 +1,3 @@
-# import os
-# import httpx
-# from dotenv import load_dotenv
-
-# load_dotenv()
-# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-# GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent"
-
-
-# async def get_chatbot_response(prompt: str) -> str:
-#     headers = {"Content-Type": "application/json"}
-#     params = {"key": GEMINI_API_KEY}
-#     data = {
-#         "contents": [{"parts": [{"text": prompt}]}]
-#     }
-#     async with httpx.AsyncClient() as client:
-#         response = await client.post(GEMINI_API_URL, headers=headers, params=params, json=data)
-#         response.raise_for_status()
-#         result = response.json()
-#         return result["candidates"][0]["content"]["parts"][0]["text"]
-
-
 import os
 import httpx
 from dotenv import load_dotenv
