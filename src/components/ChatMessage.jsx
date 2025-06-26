e563c6f1aa48bb66206ca1e88951e33d7f064d06
Tanushka Sharma <tanushkas723@gmail.com>
2025-06-26 15:34:24 +0530

    updates

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,23 +1,3 @@
-// export default function ChatMessage({ text, isUser }) {
-//   return (
-//     <div className={`max-w-xl px-4 py-2 rounded-lg ${isUser ? 'bg-blue-100 self-end' : 'bg-gray-100 self-start'}`}>
-//       {text}
-//     </div>
-//   );
-// }
-
-
-
-// export default function ChatMessage({ text, isUser }) {
-//   return (
-//     <div
-//   className={`max-w-xl px-4 py-2 rounded-lg ${isUser ? 'bg-blue-100 self-end' : 'bg-gray-100 self-start'}`}
-//   dangerouslySetInnerHTML={{ __html: text.replace(/\n/g, '<br />') }}
-// />
-//   );
-// }
-
-
 import ReactMarkdown from 'react-markdown';
 
 export default function ChatMessage({ text, isUser }) {
